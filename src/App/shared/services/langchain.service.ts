0c7944eabb1b65bdbfe021df91829ad13b66f4be
franco <francojmprogramacion@gmail.com>
2024-07-29 16:15:41 -0300

    fix companyagent

--- src/App/shared/services/langchain.service.ts
+++ src/App/shared/services/langchain.service.ts
@@ -126,7 +126,7 @@ export class LangchainService {
       name: 'get_info_company',
       description: 'Retorma información de la empresa.',
       func: async (input: string) => {
-        return 'company_name: rodriguez corp | type: empresa de modelaje coreana | models: la empresa cuenta con 50 modelos coreanas.';
+        return 'nombre: Restaurant Enrique cavill | type: restaurant de comida argentina con más de 25 años de experiencia | Especialidades: milanesas y empanadas tucumanas';
       },
     });
 
